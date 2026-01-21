a59a6347d19584c55139a8232c517d5829c14dcf
DongheonYeon <kite2731@gmail.com>
2026-01-21 16:16:44 +0900

    fix: import errors

--- arna_3d/processing/segmentation/__init__.py
+++ arna_3d/processing/segmentation/__init__.py
@@ -1,10 +1,11 @@
 """세그멘테이션 처리 모듈"""
 
-from .preprocessing import preprocess_segmentation, apply_fat_dilation
+from .preprocessing import preprocess_segmentation, apply_fat_dilation, merge_tumor_to_kidney
 from .morphology import get_largest_component
 
 __all__ = [
     "preprocess_segmentation",
     "apply_fat_dilation",
+    "merge_tumor_to_kidney",
     "get_largest_component",
 ]
